d351a0100190c32daca2cdf3f876089bcb875751
negro-vinilo <52874360+negro-vinilo@users.noreply.github.com>
2023-11-10 17:51:34 +1100

    section 4 advanced component types

--- 04-advanced-components/02-alternate-project/src/App.tsx
+++ 04-advanced-components/02-alternate-project/src/App.tsx
@@ -1,5 +1,29 @@
+import { useRef } from 'react';
+
+import Input from './components/Input.tsx';
+import Form, { type FormHandle } from './components/Form.tsx';
+import Button from './components/Button.tsx';
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  const customForm = useRef<FormHandle>(null);
+
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+    customForm.current?.clear();
+  }
+
+  return (
+    <main>
+      <Form onSave={handleSave} ref={customForm}>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
+    </main>
+  );
 }
 
 export default App;
