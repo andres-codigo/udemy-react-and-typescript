55a4352e38c65db100876191b77fd58e5f504c29
negro-vinilo <52874360+negro-vinilo@users.noreply.github.com>
2023-11-10 11:59:55 +1100

    folder set-up and section 2 folder creation andapplicable files moved into it

---
+++ 02-ts-basics-and-core-concepts/first-app.js
@@ -0,0 +1,13 @@
+var userName = 'Andres';
+userName = 'Andres';
+var userAge = 34;
+var isValid = true;
+var userID = 'abc1';
+userID = 123;
+var user;
+user = {
+    name: 'Andres',
+    age: 34,
+    isAdmin: true,
+    id: 'abc',
+};
